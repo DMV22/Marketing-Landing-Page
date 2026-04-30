d3edf2206a2b6843c495c1d7182bfa7a2d35c859
DMV22 <maksdivnich12@gmail.com>
2026-04-30 11:34:42 +0300

    feat: implement logo marquee section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Hero from "./components/sections/Hero/Hero";
 import Navbar from "./components/sections/Navbar/Navbar";
 import Container from "./components/layout/Container/Container";
+import Social from "./components/sections/Social/Social";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Container>
         <Hero />
+        <Social />
       </Container>
     </div>
   );
