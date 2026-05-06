7926fec08259a63c613c7c909d185904392b64c0
DMV22 <maksdivnich12@gmail.com>
2026-05-07 02:33:05 +0300

    feat: create Contact section with contact info items and responsive styles

---
+++ src/components/sections/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import { contactData } from './contact.data'
+import styles from './Contact.module.scss'
+
+
+export default function Contact() {
+  return (
+    <section className={styles.contact}>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Talk to our team</h2>
+        <p className={styles.description}>We're committed to delivering the support you require to make your experience as smooth as possible.</p>
+
+        <ul className={styles.menu}>
+          {contactData.map(({ id, image, link, text }) => (
+            <li key={id} className={styles.item}>
+              <img src={image} alt="" />
+              <a href={link} target="_blank" rel="noreferrer" className={styles.link}>
+                {text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
